05c6171581bdc04a36e7edc7f2ca251d4bb0c9c1
ilzge <fluxway@163.com>
2024-05-15 21:22:55 +0800

    Update usecarqx.js

--- usecarqx.js
+++ usecarqx.js
@@ -1,5 +1,3 @@
-[filter_local]
-host-keyword,patentexam,direct
 [rewrite_local]
 https://app2.patentexam.com.cn/nbtaxi/api/punch/queryWorkPointList url response-body "longitude":"116.307095","latitude":"39.827151","rangee":" response-body "longitude":"116.307095","latitude":"39.827151","rangee":"300000000000
 [mitm] 
