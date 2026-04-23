b449fcf52b24e32fe63ac46d1ba88964770fa07e
ilzge <fluxway@163.com>
2026-04-23 19:36:19 +0800

    Create xinusecarbetterqx.js

---
+++ xinusecarqx/xinusecarbetterqx.js
@@ -0,0 +1,8 @@
+[rewrite_local]
+ 
+^https:\/\/app\.patentexam\.com\.cn\/prod-api\/business\/attendance_location\/selectEnabledLocations url response-body "longitude":116\.30\d*,"latitude":39\.82\d*,"radius":" response-body "longitude":116.306981,"latitude":39.826623,"radius":"300000000000
+
+^https:\/\/app\.patentexam\.com\.cn\/prod-api\/business\/attendance_record\/punch url script-request-body https://raw.githubusercontent.com/ilzge/usecarmod/refs/heads/main/xinusecarqx/xinusecarbetter.js
+
+[mitm] 
+hostname = %APPEND% *.patentexam.com.cn
