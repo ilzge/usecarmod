691b18739728a10a8b23a82d13c8ad6127077f4f
ilzge <fluxway@163.com>
2026-04-23 18:11:43 +0800

    Update xinusecarqx.js

--- xinusecarqx/xinusecarqx.js
+++ xinusecarqx/xinusecarqx.js
@@ -1,6 +1,6 @@
 [rewrite_local]
  
-^https:\/\/app\.patentexam\.com\.cn\/prod-api\/business\/attendance_location\/selectEnabledLocations url response-body "longitude":116.306981,"latitude":39.826623,"radius":" response-body "longitude":116.306981,"latitude":39.826623,"radius":"300000000000
+^https:\/\/app\.patentexam\.com\.cn\/prod-api\/business\/attendance_location\/selectEnabledLocations url response-body "longitude":116\.30\d*,"latitude":39\.82\d*,"radius":" response-body "longitude":116.306981,"latitude":39.826623,"radius":"300000000000
 
 ^https:\/\/app\.patentexam\.com\.cn\/prod-api\/business\/attendance_record\/punch url script-request-body https://raw.githubusercontent.com/ilzge/usecarmod/refs/heads/main/xinusecarqx/xinusecarrandomqx.js
 
