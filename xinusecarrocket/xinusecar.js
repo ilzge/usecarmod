1b5d3f064bf3d1c4520917fd3ebb1824614b216b
ilzge <fluxway@163.com>
2026-04-23 19:49:31 +0800

    Update xinusecar.js

--- xinusecarrocket/xinusecar.js
+++ xinusecarrocket/xinusecar.js
@@ -1,2 +1,3 @@
-var body = $response.body.replace(/"longitude":116.307007,"latitude":39.826676,"radius":"/g, '"longitude":116.307007,"latitude":39.826676,"radius":"33000000000000')
+var body = $response.body.replace(/"longitude":116\.30\d*,"latitude":39\.82\d*,"radius":"[^"]*"/g, '"longitude":116.307007,"latitude":39.826676,"radius":"33000000000000"');
+
 $done({ body });
